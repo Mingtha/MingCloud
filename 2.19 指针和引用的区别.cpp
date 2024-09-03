b949137779fe1c055258bbd030c764e188234cca
Mingtha <95164579+Mingtha@users.noreply.github.com>
2024-09-03 17:19:25 +0800

    Update 2.19 指针和引用的区别.cpp

--- 2.19 指针和引用的区别.cpp
+++ 2.19 指针和引用的区别.cpp
@@ -1,2 +1,1 @@
-
-https://developer.huawei.com/consumer/cn/doc/harmonyos-guides/a1_u7406_u5e94_u7528_u62e5_u6709_u7684_u72b6_u6001-0000001774119946
+https://gitee.com/openharmony/docs/tree/master/zh-cn/application-dev
