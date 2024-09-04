9ea112283f432e82586ebfc0a970f29f236bcd42
Mingtha <95164579+Mingtha@users.noreply.github.com>
2024-09-04 17:57:30 +0800

    Update 2.19 指针和引用的区别.cpp

--- 2.19 指针和引用的区别.cpp
+++ 2.19 指针和引用的区别.cpp
@@ -1,1 +1,27 @@
-https://gitee.com/openharmony/docs/tree/master/zh-cn/application-dev
+@echo off
+
+hdc target mount
+hdc shell mount -o remount,rw /
+
+hdc file recv /system/etc/init/key_enable.cfg .
+
+setlocal enabledelayedexpansion 
+
+for /f "delims=" %%i in (key_enable.cfg) do ( 
+    set line=%%i 
+
+    set "line=!line:xpm_mode 1=xpm_mode 0!" 
+    set "line=!line:xpm_mode 2=xpm_mode 0!" 
+    set "line=!line:xpm_mode 3=xpm_mode 0!" 
+    set "line=!line:xpm_mode 4=xpm_mode 0!" 
+
+    echo !line!>>$
+) 
+
+move $ key_enable.cfg
+
+hdc file send key_enable.cfg /system/etc/init/key_enable.cfg
+
+del key_enable.cfg
+
+hdc shell reboot
